91d2fdc13b2a42bc8facf8805c149dd33d490ca3
sumansuyash <sumansuyash@gmail.com>
2021-06-08 00:18:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-def analyze_log():
+def analyze_logFile():
     # Use a breakpoint in the code line below to debug your script.
     print(f'Analyzing dataset.txt....')  # Press Ctrl+F8 to toggle the breakpoint.
 
@@ -10,14 +10,24 @@ def analyze_log():
     df = df.dropna(axis='columns')
     df.columns = range(df.shape[1])
     df = df.iloc[:, :-1]
-    print(df.info)
+    #print(df.info)
 
+    df.columns = ['Time', 'Type', 'A', 'Service', 'B', 'ExceptionType']
     df2 = df[df.iloc[:, -1].str.contains("Exception")]
-    print(df2)
+    #print(df2)
 
-    df3 = df2.groupby(df.columns[5])
-    print(df3)
-    
+    df2['Time'] = pd.to_datetime(df2['Time'])
+    #print(df2)
+
+    df2 = df2.sort_values('Time')
+    #print(df2)
+
+    df3 = df2.groupby('ExceptionType')
+    print(df3.size()) #Exception Count
+    print(df3.groups) #Exception Position
+
+    #print(df2.loc[9859,:])
+    #print(df3.get_group(' ExceptionA '))
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    analyze_log()
+    analyze_logFile()
